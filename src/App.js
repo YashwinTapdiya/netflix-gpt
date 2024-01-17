3d9131f692088ebef89f7648cd67be1c3e518845
Yashwin Tapdiya <yashwintapdiya@gmail.com>
2024-01-17 19:04:04 +0530

    added header and bg img

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import "./App.css";
+import Body from "./components/Body";
 
 function App() {
-  return <div className="text-3xl font-bold text-green-800"> Hello People</div>;
+  return (
+    <Body/>
+  )
 }
 
 export default App;
---
+++ src/components/Login.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./Header";
+
+const Login = () => {
+  return (
+    <div>
+      <Header />
+      <div>
+        <img
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/594f8025-139a-4a35-b58d-4ecf8fdc507c/d3c4e455-f0bf-4003-b7cd-511dda6da82a/IN-en-20240108-popsignuptwoweeks-perspective_alpha_website_large.jpg"
+          alt="back-ground-image"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Login;

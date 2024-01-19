c9b7f128476e02a12404196df0e913325d62b2c1
Yashwin Tapdiya <yashwintapdiya@gmail.com>
2024-01-19 05:39:13 +0530

    created sign-in/sign-up form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,16 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 
 const Login = () => {
+  const [isSignInFrom, setIsSignInFrom] = useState(false);
+
+  const toggleSignInForm = () => {
+    setIsSignInFrom(!isSignInFrom);
+  };
+
   return (
     <div>
       <Header />
-      <div>
+      <div className="absolute">
         <img
           src="https://assets.nflxext.com/ffe/siteui/vlv3/594f8025-139a-4a35-b58d-4ecf8fdc507c/d3c4e455-f0bf-4003-b7cd-511dda6da82a/IN-en-20240108-popsignuptwoweeks-perspective_alpha_website_large.jpg"
           alt="back-ground-image"
         />
       </div>
+      <form className="absolute p-12 bg-black w-3/12 my-36 mx-auto right-0 left-0 text-white bg-opacity-80">
+        <h1 className="font-bold text-3xl py-4">
+          {isSignInFrom === true ? "Sign In" : "Sign Up"}
+        </h1>
+        {!isSignInFrom && (
+          <input
+            type="text"
+            placeholder="Full Name"
+            className="p-4 my-4 w-full bg-gray-700"
+          />
+        )}
+        <input
+          type="text"
+          placeholder="Email Address"
+          className="p-4 my-4 w-full bg-gray-700"
+        />
+        <input
+          type="passpord"
+          placeholder="Password"
+          className="p-4 my-4 w-full bg-gray-700"
+        />
+        <button className="p-4 my-6 bg-red-700 w-full rounded-lg">
+          {isSignInFrom === true ? "Sign In" : "Sign Up"}
+        </button>
+        <p className="py-4 cursor-pointer" onClick={toggleSignInForm}>
+          {isSignInFrom === true
+            ? "New To Netflix? Sign-Up Now"
+            : "Already registered? Sign-In Now"}
+        </p>
+      </form>
     </div>
   );
 };

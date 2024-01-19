1d64bf3b2f806135bba3955c841dd6c90fcc0ec3
Yashwin Tapdiya <yashwintapdiya@gmail.com>
2024-01-19 20:56:30 +0530

    using tmdb api to fetch movies

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,3 +3,12 @@ export const NETFLIX_LOGO =
 
 export const BACK_IMG =
   "https://assets.nflxext.com/ffe/siteui/vlv3/594f8025-139a-4a35-b58d-4ecf8fdc507c/d3c4e455-f0bf-4003-b7cd-511dda6da82a/IN-en-20240108-popsignuptwoweeks-perspective_alpha_website_large.jpg";
+
+export const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlN2EwOTRmZTJmYjZjZDVlNzAxZTJiNzNmMzRhZWQ1NyIsInN1YiI6IjY1YWE5MDFmNmFmOGY4MDEzMmJlNzlkYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.xuBSkRPBUfNnfgIkuBARUs3enzHoRJ76xAfoBs-DxHg",
+  },
+};

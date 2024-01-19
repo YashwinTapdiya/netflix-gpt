2aa60e03482d26183465880d7e2ec0d0483005a4
Yashwin Tapdiya <yashwintapdiya@gmail.com>
2024-01-19 20:29:41 +0530

    unsubscribe to onAuthStateChanged callback

--- src/components/Header.js
+++ src/components/Header.js
@@ -18,7 +18,7 @@ const Header = () => {
       });
   };
   useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
         const { uid, email, displayName } = user;
         dispatch(
@@ -35,6 +35,7 @@ const Header = () => {
         navigate("/");
       }
     });
+    return () => unsubscribe();
   }, []);
 
   return (

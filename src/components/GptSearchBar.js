ba1c3fc25146461f2163f342f8aa92b409051bd3
Yashwin Tapdiya <yashwintapdiya@gmail.com>
2024-01-21 00:21:22 +0530

    integrated openai api to fetch movie info

--- src/components/GptSearchBar.js
+++ src/components/GptSearchBar.js
@@ -1,19 +1,44 @@
 import React from "react";
 import lang from "../utils/languageConstants";
 import { useSelector } from "react-redux";
+import { useRef } from "react";
+import openai from "../utils/openai";
 
 const GptSearchBar = () => {
-  const language = useSelector(store => store.config.lang);
+  const language = useSelector((store) => store.config.lang);
+  const searchText = useRef(null);
+
+  const handleGptSearchClick = async () => {
+    console.log(searchText.current.value)
+    const gptQuery =
+      "Act as a Movie Recommendation system and suggest some movies for the query : " +
+      searchText.current.value +
+      ". only give me names of 5 movies, comma seperated like the example result given ahead. Example Result: Batman, Sholey, Dhol, Gone Girl, Garam Masala";
+    const gptResults = await openai.chat.completions.create({
+      messages: [{ role: "user", content: gptQuery }],
+      model: "gpt-3.5-turbo",
+    });
+    console.log(gptResults.choices);
+  };
 
   return (
     <div className="pt-[10%] flex justify-center">
-      <form className="w-1/2 bg-black grid grid-cols-12">
+      <form
+        className="w-1/2 bg-black grid grid-cols-12"
+        onSubmit={(e) => e.preventDefault()}
+      >
         <input
+          ref={searchText}
           type="text"
           className="p-4 m-4 col-span-9"
           placeholder={lang[language].gptSearchPlaceholder}
         />
-        <button className="col-span-3 m-4 py-2 px-4 bg-red-700 text-white rounded-lg">{lang[language].search}</button>
+        <button
+          className="col-span-3 m-4 py-2 px-4 bg-red-700 text-white rounded-lg"
+          onClick={handleGptSearchClick}
+        >
+          {lang[language].search}
+        </button>
       </form>
     </div>
   );
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -17,3 +17,5 @@ export const SUPPORTED_LANGUAGES = [
   { identifier: "hindi", name: "Hindi" },
   { identifier: "spanish", name: "Spanish" },
 ];
+
+export const OPENAI_KEY = "";
